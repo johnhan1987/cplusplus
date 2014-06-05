26a74aaee9f623b9c07f61cdc5f37aeae21238cf
john <john@john-VirtualBox.(none)>
2014-06-05 15:13:45 -0400

    add project 3

---
+++ project_3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//for loops
+	cout << "for loop: \n" << endl;
+	int i=1;
+	int sum=0;
+	for(i=1; i<=5; i++){
+		sum = sum + i;
+	}
+	cout << "The sum is " << sum << "\n"<< endl;
+	
+	//if statement
+
+	return 0;
+}
