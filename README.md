e95d60665066f05da282e3ca195e98f7bd1d2be0
john <john@john-VirtualBox.(none)>
2014-06-05 14:13:54 -0400

    add project 2 and a very good makefile

--- README.md
+++ README.md
@@ -2,6 +2,17 @@ cplusplus
 =========
 ##project 1: 
 compile and run simple cpp code with g++
+
 ###command: 
 * g++ -o testobj test.cpp
 * ./testobj
+
+
+##project 2:
+This project includes a very good makefile! It can be used in any projects, without modifying the makefile itself.
+In this project, we use the makefile to compile the c++ project and run.
+
+###command:
+* make
+* make clean
+* ./main
---
+++ project_2/Date.h
@@ -0,0 +1,26 @@
+#ifndef DATE_H
+#define DATE_H
+ 
+#include <iostream>
+using namespace std;
+
+class Date
+{
+private:
+    int m_nMonth;
+    int m_nDay;
+    int m_nYear;
+ 
+    Date() { } // private default constructor
+ 
+public:
+    Date(int nMonth, int nDay, int nYear);
+ 
+    void SetDate(int nMonth, int nDay, int nYear);
+ 
+    int GetMonth() { return m_nMonth; cout<<m_nMonth<<endl;}
+    int GetDay()  { return m_nDay; cout<<m_nDay<<endl;}
+    int GetYear() { return m_nYear; cout<<m_nYear<<endl;}
+};
+ 
+#endif
